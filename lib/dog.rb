54629770381ab450230bb2573cdfc84f7d07c3f0
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-12-16 05:57:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,19 +2,19 @@
 class Dog
   def initialize(name="Fido")
     @name = name
-    
-  end
-  
 
-  def bark  
-    puts 'woof!'  
-  end  
-  
+  end
+
+
+  def bark
+    puts 'woof!'
+  end
+
   def name=(name)
     @name= name
   end
- 
+
   def name
     @name
   end
-end  
+end
